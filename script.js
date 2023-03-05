902f3fcc64022b84d0b79a50ee789bc3f0e3d1c4
Maja Zimnoch <majamargareta@Majas-MacBook-Air.local>
2023-03-05 14:55:15 +0100

    working on the desktop version whihc is in progress. added a JS effect working on the scroll to some sections

--- script.js
+++ script.js
@@ -20,3 +20,17 @@ window.addEventListener("mousemove", (e) => {
         })
         })
     })
+
+    const observer = new IntersectionObserver((entries) =>  {
+        entries.forEach((entry) => {
+            console.log(entry)
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+    
+            } else {
+                entry.target.classList.remove('show');
+            }
+        });
+    });
+    const hiddenElements = document.querySelectorAll(".hidden");
+    hiddenElements.forEach((el) => observer.observe(el));
